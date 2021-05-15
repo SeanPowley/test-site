5cd086b52df943e2ed938878a3ee2b007894c667
Sean Powley <seanpowley@yahoo.com>
2021-05-15 19:01:55 -0400

    foxy-grey

--- scripts-folder/main.js
+++ scripts-folder/main.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images-folder/test-site.jpg1.jpg') {
+        myImage.setAttribute('src','images-folder/grey-fox.jpg');
+    } else {
+        myImage.setAttribute('src','images-folder/test-site.jpg1.jpg');
+    }
+}    
